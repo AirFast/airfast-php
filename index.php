317daf755432bab20a22addf063c9f5ac8fcc51f
AirFast <airfast.88@gmail.com>
2020-10-14 21:15:03 +0300

    PHP Data Object

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 error_reporting( E_ALL );
 
+//$url = $_SERVER['REQUEST_URI'];
+//$_SERVER['REQUEST_URI'] = '/admin/post';
+//$arr = parse_url($url);
+//$path = explode('/', $arr['path']);
+//var_dump($arr);
+//
+//die();
+
 require_once __DIR__ . '/autoloader.php';
 
 $ctrl = isset( $_GET['ctrl'] ) ? $_GET['ctrl'] : 'News';
